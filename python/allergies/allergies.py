bfad88501ca01266c0353b9c87ffbb3cc5b32b58
george-cm <george.murga+github@gmail.com>
2021-01-08 17:05:11 +0200

    Updated one method per the updated exercise instructions (it changed after 2 years)

--- python/allergies/allergies.py
+++ python/allergies/allergies.py
@@ -12,16 +12,16 @@ class Allergies(object):
 
     def __init__(self, score):
         self.score = score
-        self.allergic_to = []
+        self._allergic_to = []
         for i, allergen in enumerate(Allergies.allergens):
             if bool(score & 2**i):
-                self.allergic_to.append(allergen)
+                self._allergic_to.append(allergen)
 
-    def is_allergic_to(self, item):
-        if item in self.allergic_to:
+    def allergic_to(self, item):
+        if item in self._allergic_to:
             return True
         return False
 
     @property
     def lst(self):
-        return self.allergic_to
+        return self._allergic_to
