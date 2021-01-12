73bb81d4a0003533e1ce469673c97a4b84f97ae6
george-cm <george.murga+github@gmail.com>
2021-01-12 09:14:27 +0200

    Solution 2

--- c/armstrong-numbers/src/armstrong_numbers.c
+++ c/armstrong-numbers/src/armstrong_numbers.c
@@ -3,7 +3,7 @@
 #include <math.h>
 #include "armstrong_numbers.h"
 
-bool is_armstrong_number(int candidate)
+bool is_armstrong_number1(int candidate)
 {
     int length = snprintf(NULL, 0, "%d", candidate);
     char *s_candidate = malloc(length + 1);
@@ -14,6 +14,39 @@ bool is_armstrong_number(int candidate)
         int num = s_candidate[i] - '0';
         sum += pow(num, length);
     }
+    if (sum == candidate)
+    {
+        return true;
+    }
+    else
+    {
+        return false;
+    }
+}
+
+bool is_armstrong_number(int candidate)
+{
+    int digit;
+    int remaining;
+    int sum = 0;
+    int *digits = malloc(sizeof(int));
+    int length = 0;
+
+    remaining = candidate;
+    while (remaining > 0)
+    {
+        digit = remaining % 10;
+        remaining = remaining / 10;
+        length += 1;
+        digits = realloc(digits, length * sizeof(int));
+        *(digits + length - 1) = digit;
+    }
+
+    for (int i = 0; i < length; i++)
+    {
+        sum += pow(*(digits + i), length);
+    }
+
     if (sum == candidate)
     {
         return true;
