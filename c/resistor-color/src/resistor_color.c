79c59c6809eef058be31cb3bf915d2f13bb05d24
george-cm <george.murga+github@gmail.com>
2021-01-16 11:25:01 +0200

    Solution 3

--- c/resistor-color/src/resistor_color.c
+++ c/resistor-color/src/resistor_color.c
@@ -9,7 +9,12 @@ int color_code(resistor_band_t color)
 resistor_band_t *colors(void)
 {
     static resistor_band_t all_colors[COUNT];
-    for (resistor_band_t i = 0; i < COUNT; ++i)
-        all_colors[i] = i;
+    static int initialized = 0;
+    if (!initialized)
+    {
+        initialized = 1;
+        for (resistor_band_t i = 0; i < COUNT; ++i)
+            all_colors[i] = i;
+    }
     return all_colors;
 }
