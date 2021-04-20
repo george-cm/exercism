d77e34d1d5909495a77ac1cca60be0e617956551
george-cm <george.murga+github@gmail.com>
2021-04-20 19:31:45 +0300

    Solution 1

--- python/error-handling/error_handling.py
+++ python/error-handling/error_handling.py
@@ -1,14 +1,21 @@
 def handle_error_by_throwing_exception():
-    pass
-
+    raise Exception("Throwing exception")
+    
 
 def handle_error_by_returning_none(input_data):
-    pass
+    try:
+        return int(input_data)
+    except Exception:
+        return None
 
 
 def handle_error_by_returning_tuple(input_data):
-    pass
+    try:
+        return True, int(input_data)
+    except Exception:
+        return False, None
 
 
-def filelike_objects_are_closed_on_exception(filelike_object):
-    pass
+def filelike_objects_are_closed_on_exception(filelike_object):    
+    filelike_object.close()
+    filelike_object.do_something() 
